84916d69c97fe6c28fd5051d4582a473a8e00fc0
robert5313 <kiptoorobert@zetech.ac.ke>
2023-01-30 21:01:41 -0800

    my tasks

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -81,5 +81,5 @@ class Rectangle:
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
         type(self).number_of_instances -= 1
-        print("New rectangle deleted...")
+        print("Bye rectangle...")
 
