05519d17ff2c7aec6d1b0f27eac725593434c9a5
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-04-14 13:35:16 +0300

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (width, height) {
-    this.width = width;
-    this.height = heght;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 
