d148ea25c43621fcb2f7fd11ab9de6941753bbd5
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-04-28 00:57:12 +0300

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays all HTTP methods the server will accept
+# Script that sends a JSON POST request to a URL passed as the first argument
 curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
