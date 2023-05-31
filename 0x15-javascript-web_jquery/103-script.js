a48dc6da5c5b790bdb6345053f92c6f8785ef233
robert5313 <kiptoorobert@zetech.ac.ke>
2023-05-31 21:12:18 +0000

    jquery task

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,11 @@
+
+cument').ready(function () {
+  $('INPUT#btn_translate').click(translate);
+  $('INPUT#language_code').focus(function () {
+    $(this).keydown(function (e) {
+      if (e.keyCode === 13) {
+        translate();
+      }
+    });
+  });
+});
